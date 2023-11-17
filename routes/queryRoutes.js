10ffa35e144e812943353b252f3dff33f1ef4e2e
Praveen Kamal <45634148+praveen-kamal@users.noreply.github.com>
2023-11-17 18:18:44 +0530

    Removed authentication for certain routes

--- routes/queryRoutes.js
+++ routes/queryRoutes.js
@@ -9,10 +9,10 @@ import verifyToken from "../middleware/loginMiddleware.js";
 const router = express.Router();
 
 router.route("/")
-    .get(verifyToken, getQueries)
+    .get(getQueries)
     .post(postQuery);
 
 router.route("/:id")
-    .delete(verifyToken, deleteQuery);
+    .delete(deleteQuery);
 
 export default router;
