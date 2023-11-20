293e8800e5c880a220e3cf782f573dd562fdcc65
Praveen Kamal <45634148+praveen-kamal@users.noreply.github.com>
2023-11-20 13:01:28 +0530

    changed server.js

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors(
     {
-        origin: ["http://localhost:3000"],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     },
