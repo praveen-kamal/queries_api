4dcee59df9346d9e382cb9be1d977723191a67ad
Praveen Kamal <45634148+praveen-kamal@users.noreply.github.com>
2023-11-20 14:33:03 +0530

    changed server.js

--- server.js
+++ server.js
@@ -11,12 +11,7 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors(
-    {
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        credentials: false,
-    },
-));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
